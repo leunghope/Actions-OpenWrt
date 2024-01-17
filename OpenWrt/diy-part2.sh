08dc24ca2938d9ec9f600a7032149111e182db23
初夏知鸣 <31916850+leunghope@users.noreply.github.com>
2024-01-17 21:46:41 +0800

    Update diy-part2.sh

--- OpenWrt/diy-part2.sh
+++ OpenWrt/diy-part2.sh
@@ -4,8 +4,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # 自定义 sh
 echo "设置lan ip"
-lan_ip='192.168.1.1' 
-sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+lan_ip='10.0.0.1' 
+sed -i "s/10.0.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 
 echo "修改时区"
 utc_name='Asia\/Shanghai' 
@@ -16,7 +16,7 @@ device_name='OpenWrt'
 sed -i "s/OpenWrt/$device_name/g" ./package/base-files/files/bin/config_generate
 
 echo "修改wifi名称"
-wifi_name=ysf
+wifi_name=Redmi_823B
 sed -i "s/OpenWrt/$wifi_name/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo "wifi默认开启"
